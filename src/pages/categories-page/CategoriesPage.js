7ca3ca4100a014a09cdb35345e465285694f18c0
Bojana Corbic <bojana.corbic@quantox.com>
2020-02-19 10:53:03 +0100

    add style

--- src/pages/categories-page/CategoriesPage.js
+++ src/pages/categories-page/CategoriesPage.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { Grid, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Paper, ButtonBase, Typography } from "@material-ui/core";
 
 import {
   fetchCategoriesMeal,
@@ -18,15 +19,114 @@ function CategoriesPage(props) {
     props.categorieInput(e.target.textContent);
   };
 
+  const useStyles = makeStyles(theme => ({
+    root: {
+      display: "flex",
+      flexWrap: "wrap",
+      minWidth: 300,
+      width: "100%"
+    },
+    image: {
+      position: "relative",
+      height: 200,
+      [theme.breakpoints.down("xs")]: {
+        width: "100% !important", // Overrides inline-style
+        height: 100
+      },
+      "&:hover, &$focusVisible": {
+        zIndex: 1,
+        "& $imageBackdrop": {
+          opacity: 0.15
+        },
+        "& $imageMarked": {
+          opacity: 0
+        },
+        "& $imageTitle": {
+          border: "4px solid currentColor"
+        }
+      }
+    },
+    focusVisible: {},
+    imageButton: {
+      position: "absolute",
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      color: theme.palette.common.white
+    },
+    imageSrc: {
+      position: "absolute",
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0,
+      backgroundSize: "cover",
+      backgroundPosition: "center 40%"
+    },
+    imageBackdrop: {
+      position: "absolute",
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0,
+      backgroundColor: theme.palette.common.black,
+      opacity: 0.4,
+      transition: theme.transitions.create("opacity")
+    },
+    imageTitle: {
+      position: "relative",
+      padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${theme.spacing(1) +
+        6}px`
+    },
+    imageMarked: {
+      height: 3,
+      width: 18,
+      backgroundColor: theme.palette.common.white,
+      position: "absolute",
+      bottom: -2,
+      left: "calc(50% - 9px)",
+      transition: theme.transitions.create("opacity")
+    }
+  }));
+
+  const classes = useStyles();
+
   const content = props.categories.map((categorie, index) => (
-    <Grid item xs={4} key={index}>
-      <Paper onClick={handleClick} style={{ textAlign: "center", padding: 20 }}>
-        <Link to={`/${categorie.idCategory}`}>{categorie.strCategory}</Link>
-      </Paper>
-    </Grid>
+    <ButtonBase
+      focusRipple
+      key={index}
+      className={classes.image}
+      focusVisibleClassName={classes.focusVisible}
+      style={{
+        width: "30%"
+      }}
+    >
+      <span
+        className={classes.imageSrc}
+        style={{
+          backgroundImage: `url(${categorie.strCategoryThumb})`
+        }}
+      />
+      <span className={classes.imageBackdrop} />
+      <span className={classes.imageButton}>
+        <Typography
+          component="span"
+          variant="subtitle1"
+          color="inherit"
+          className={classes.imageTitle}
+        >
+          {categorie.strCategory}
+          <span className={classes.imageMarked} />
+        </Typography>
+      </span>
+    </ButtonBase>
   ));
 
-  return <div>{content}</div>;
+  return <div className={classes.root}>{content}</div>;
 }
 
 const mapStateToProps = state => ({
